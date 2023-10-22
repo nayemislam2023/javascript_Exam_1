d78dab6562890b69789b422d019847a4c7c25592
Nayem Islam <143949943+nayemislam2023@users.noreply.github.com>
2023-10-22 13:34:47 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,267 @@
+//1 no ans
+
+
+
+
+
+
+//2 no ans
+
+
+
+//12 no ans
+/*
+var math = 80;
+var chemistry = 75;
+var physich = 81;
+var bangla = 86;
+
+var addition = math + chemistry + physich + bangla;
+
+var avg =  addition / 4;
+console.log("Total Average =" + avg);
+
+*/
+//13 no ans
+/*
+var x = 30; 
+var y = '23'; 
+var intNumber =  parseInt(y);
+
+var sum = x + intNumber;
+console.log("The sum is = " + sum);
+*/
+ 
+//14 no ans
+
+/*
+var names = ["Nayem", "Rakib", "Shakib"];
+console.log(names);
+
+names.typeof();
+console.log(names);
+//khun doroder data type seta check kore
+*/
+
+//15 no ans
+/*
+var num = "20";
+
+parseInt(num);
+
+console.log(num);
+
+//parseInt(); Method ara dara
+*/
+
+//16 no ans
+ //array index [0] thake suro hoi
+
+ //17 no ans
+
+ /*
+var numbers =[34, 56, 74, 24, 88, 34, 776, 34, 5];
+
+console.log(numbers);
+numbers[5]= 334;
+console.log(numbers);
+*/
+
+//18 no ans
+/*
+var numbers =[34, 56, 74, 24, 88, 34, 776, 34, 5];
+
+console.log(numbers);
+
+numbers.push(234);
+
+numbers.unshift(998);
+
+console.log(numbers);
+console.log(numbers);
+
+*/
+
+//19 no ans
+/*
+var numbers = [[1,2], [3,4], [5,6]];
+
+console.log(numbers);
+num = numbers.flat();
+console.log(num);
+*/
+
+//20 no ans
+/*
+var team1 = ['Rahim', 'Karim'];
+var team2 = ['Tanim', 'biplob', 'tareq'];
+var team3 = ['sajjal', 'arefin'];
+
+var arySum = team1 + team2 + team3;
+
+console.log('Three array = '+ arySum);
+
+*/
+//21 no ans
+/*
+var fruits = ["Banana", "Orang", "Apple", "Mango"];
+
+console.log(fruits);
+
+fruits.shift();
+console.log(fruits);
+*/
+ //22 no ans
+ /*
+ var fruits = ["Banana", "Orang", "Apple", "Mango"];
+
+ console.log(fruits);
+
+ fruits.splice(3, 0, "Lichi", "Pineapple", "Watermelon");
+
+ console.log(fruits);
+
+*/
+
+//23 no ans
+/*
+var num1 = 20;
+var num2 = 30;
+
+if(num1>num2){
+    console.log("Largest number is = " + num1);
+}
+else{
+    console.log("Largest number is = " + num2);
+}
+
+*/
+
+//24 no ans
+/*
+var num1 = 40;
+var num2  = 30;
+var num3 = 50;
+
+var totalNumber = num1 + num2;
+
+var addition = totalNumber + num3;
+var sub = totalNumber - num3;
+var mul = totalNumber * num3;
+var divi = totalNumber / num3;
+
+console.log("Addition " + addition);
+console.log(" Subtrat " + sub);
+console.log("Multi " + mul);
+console.log("Divition " + divi);
+*/
+//25 no ans
+/*
+var penQuantity  = 3;
+
+var totalPen = 10;
+
+var totalPenPrice = 185;
+
+var perPrice = totalPenPrice / totalPen;
+
+var penQuantityprice = penQuantity * perPrice;
+
+console.log("Total price = " + penQuantityprice);
+
+*/
+
+//26 no ans
+/*
+var nunb = 80;
+
+var parcent = 20;
+
+var parcenttage = (nunb * parcent)/ 100;
+
+console.log("Partange " + parcenttage);
+
+*/
+
+//28 no ans
+var amr_kache_ace = 500;
+var singgara = 20;
+var somocha = 15;
+var bon_ruty = 10;
+var vargger = 60;
+
+var totalBrayerprice = singgara + somocha + bon_ruty + vargger;
+
+var remaider = amr_kache_ace / totalBrayerprice;
+
+console.log(remaider);
+
+
+
+//29 no ans
+/*
+var marks = 30;
+
+if(marks>100 || marks<0){
+    console.log("Invalid marks");
+}
+else if(marks >= 90 && marks <= 100){
+    console.log("S Grade");
+}
+else if(marks >= 80 && marks <= 90){
+    console.log("A Grade");
+}
+else if(marks >= 70 && marks <= 80){
+    console.log("B Grade");
+}
+else if(marks >= 60 && marks <= 70){
+    console.log("C Grade");
+}
+else if(marks >= 50 && marks <= 60){
+    console.log("D Grade");
+}
+else if(marks >= 40 && marks <= 50){
+    console.log("E Grade");
+}
+else{
+    console.log("Student has failed");
+}
+*/
+
+//30 no ans
+/*
+Sub: somoyer vest use------
+
+=>protita somoy porita second amder jibone khoboy mulloban.bises kore dara computer user and web designer.
+ ja kiccu kora huk na keno computer and vises kore programmimg ar upor minimux 8 gonta somoy ar vest use korbo.
+ insallaha.
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
